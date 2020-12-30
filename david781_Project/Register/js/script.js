b2371667696f65104e9c0bf3b155566ac1778700
Torfa <Alexandre.Tison@etu.univ-littoral.fr>
2020-12-30 09:58:28 +0100

    David's Project

---
+++ david781_Project/Register/js/script.js
@@ -0,0 +1,23 @@
+/*Fonction qui récupére le langage du navigateur et qui
+Affiche une page en Français si le langage du navigateur est FR
+Sinon affiche la page originale (en Anglais)*/
+
+/*Function which retrieves the browser language and which
+Displays a page in French if the browser language is FR
+Otherwise display the original page (in English)*/
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let userLang = navigator.language;
+    let vfPlacement = document.getElementById("frenchVersion");
+    let voPlacement = document.getElementById("originalVersion");
+    if (userLang == "fr" || userLang == "fr-FR"){
+        vfPlacement.style.display = "block";
+        voPlacement.style.display = "none";
+        document.title = 'ChefProg - Accueil';
+    }
+        else {
+            vfPlacement.style.display = "none";
+            voPlacement.style.display = "block";
+            document.title = 'ChefProg - Home';
+        }
+});
